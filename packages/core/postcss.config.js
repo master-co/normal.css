0b9fe9a632648809e19d7e7180bd8b018afe6ec2
Aron <i@aron.tw>
2024-05-03 00:08:55 +0800

    New: Add `@layer base` to lower the precedence for all rules

--- packages/core/postcss.config.js
+++ packages/core/postcss.config.js
@@ -1,7 +1,11 @@
+
 module.exports = {
     plugins: [
         require('cssnano'),
         require('postcss-combine-duplicated-selectors'),
         require('postcss-import'),
+        require('postcss-assign-layer').default([
+            { include: '**/*.css', layerName: 'base' }
+        ])
     ]
 }
